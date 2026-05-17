6f94c2a7871e8924cfdfacb13d5c4baefe324333
Sameer kadyan <gulshansameer51@gmail.com>
2026-05-17 13:36:39 +0530

    Authorization barear

--- src/api/profileApi.js
+++ src/api/profileApi.js
@@ -1,17 +1,35 @@
 export const getProfile = async () => {
+
+  const token = localStorage.getItem("token");
+
   return fetch(
-    "https://backend-server-arp0.onrender.com/api/auth/profile"
+    "https://backend-server-arp0.onrender.com/api/auth/profile",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
   );
 };
 
 export const uploadProfilePhoto = async (file) => {
+
+  const token = localStorage.getItem("token");
+
   const formData = new FormData();
+
   formData.append("profilePhoto", file);
 
   return fetch(
     "https://backend-server-arp0.onrender.com/api/auth/upload-profile",
     {
       method: "POST",
+
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+
       body: formData,
     }
   );
