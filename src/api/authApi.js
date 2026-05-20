2dc3139a94c00b6d7f189bd1d8ba2fbdebc37d42
Sameer kadyan <gulshansameer51@gmail.com>
2026-05-20 14:39:10 +0530

    authAPI.js file correct ki

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,7 @@
 export const loginUser = async (email, password) => {
   const response = await fetch("https://backend-server-arp0.onrender.com/api/auth/login", {
     method: "POST",
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
