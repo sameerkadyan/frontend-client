625a84570eade7820484b14d37cad467f53c0e45
Sameer kadyan <gulshansameer51@gmail.com>
2026-05-22 04:32:29 +0530

    layout fix

--- src/pages/student/Profile.jsx
+++ src/pages/student/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ProfileNavbar from "../components/profile/ProfileNavbar";
-import { useProfile } from "../hooks/useProfile";
+import ProfileNavbar from "../../components/profile/ProfileNavbar";
+import { useProfile } from "../../hooks/useProfile";
 
 import "../style/profile.css";
 
