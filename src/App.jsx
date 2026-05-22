ff63d0b69253366a3a86bbf57208d91df29259e3
Sameer kadyan <gulshansameer51@gmail.com>
2026-05-23 01:22:54 +0530

    Fix layout after login

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,48 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  useLocation,
 } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-// import ProfileNavbar from "./components/profile/ProfileNavbar";
 
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import VerifyOTP from "./pages/auth/VerifyOTP";
-import StudentLayout from "./layouts/StudentLayout";
 
-// import Dashboard from "./pages/student/Dashboard";
-// import Profile from "./pages/student/Profile";
-// import Students from "./pages/student/Students";
+import StudentLayout from "./layouts/StudentLayout";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 
-return App()(
-  <BrowserRouter>
-    <Router>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="verify-otp" element={<VerifyOTP />} />
+function App() {
+  return (
+    <BrowserRouter>
 
-      <Route
-      path="/students"
-      element={
-        <ProtectedRoute>
+      <Navbar />
 
-        </ProtectedRoute>
-      }
-      />
+      <Routes>
 
+        <Route path="/" element={<Home />} />
 
-    </Router>
-  </BrowserRouter>
-);
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/verify-otp" element={<VerifyOTP />} />
+
+        <Route
+          path="/students"
+          element={
+            <ProtectedRoute>
+              <StudentLayout />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
 
 export default App;
